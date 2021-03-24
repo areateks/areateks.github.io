13f6b1110c3f8fbb0419f1e5759023b1f508f0ac
areateks <78294404+areateks@users.noreply.github.com>
2021-03-25 05:38:48 +0700

    add

--- seo-image-generator/img.js
+++ seo-image-generator/img.js
@@ -4,9 +4,8 @@ if (window.location.search) {
   var url = config.url;
   var googleurl = url.replace(/1.bp.blogspot.com/g, 'lh4.googleusercontent.com');
   var title = config.title;
-  var alt = alt.title;
-  var title = config.title;
-  var caption = caption.title;
+  var alt = config.alt;
+  var caption = caption.caption;
   var imageresult = '<figure><img src="' + googleurl + '" title="' + title + '" alt="' + alt + '" /><figcaption>' + caption + '</figcaption></figure>';
   img.innerText = imageresult;
 }
