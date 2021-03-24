170599aafcccc3ac49c7caef06cd9998ea44d870
areateks <78294404+areateks@users.noreply.github.com>
2021-03-25 05:43:10 +0700

    Update img.js

--- seo-image-generator/img.js
+++ seo-image-generator/img.js
@@ -5,7 +5,7 @@ if (window.location.search) {
   var googleurl = url.replace(/1.bp.blogspot.com/g, 'lh4.googleusercontent.com');
   var title = config.title;
   var alt = config.alt;
-  var caption = caption.caption;
+  var caption = config.caption;
   var imageresult = '<figure><img src="' + googleurl + '" title="' + title + '" alt="' + alt + '" /><figcaption>' + caption + '</figcaption></figure>';
   img.innerText = imageresult;
 }
