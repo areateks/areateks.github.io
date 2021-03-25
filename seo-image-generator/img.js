5b72d449feb6126972da2a16be22e9a25a627b63
areateks <78294404+areateks@users.noreply.github.com>
2021-03-25 20:46:19 +0700

    Update img.js

--- seo-image-generator/img.js
+++ seo-image-generator/img.js
@@ -4,8 +4,11 @@ if (window.location.search) {
   var url = config.url;
   var googleurl = url.replace(/1.bp.blogspot.com/g, 'lh4.googleusercontent.com');
   var title = config.title;
+  var imgtitle = title.replace(/\+/g, ' ');
   var alt = config.alt;
+  var imgalt = alt.replace(/\+/g, ' ');
   var caption = config.caption;
-  var imageresult = '<figure><img src="' + googleurl + '" title="' + title + '" alt="' + alt + '" /><figcaption>' + caption + '</figcaption></figure>';
+  var imgcaption = caption.replace(/\+/g, ' ');
+  var imageresult = '<figure><img src="' + googleurl + '" title="' + imgtitle + '" alt="' + imgalt + '" /><figcaption>' + imgcaption + '</figcaption></figure>';
   img.innerText = imageresult;
 }
